b5baee1c994238b049f6516b97deaf422f686214
ZaimAbz <zaimabodi@gmail.com>
2019-07-12 16:30:35 +0100

    creating ProgressPage component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from "react";
 import "./App.css";
 import { WebClient } from "@slack/client";
-import ProgressBar from "./Components/ProgressBar";
-import logo from "./logo-cyf.png";
-import Button from "./Components/Button";
-import Barchart from "./Components/Barchart";
+
+import ProgressPage from "./Components/ProgressPage";
 function App() {
   return (
     <div className="container-fluid">
-      <div className="d-flex  align-items-center justify-content-center justify-content-lg-around ">
-        <Button title="Performance" />
-        <div className="d-flex flex-column align-items-center">
-          <img src={logo} style={{ width: 300, marginBottom: 10 }} />
-          <h1>Today</h1>
-          <ProgressBar />
-          <Barchart />
-        </div>
-        <Button title="Set a target" />
-      </div>
+      <ProgressPage />
     </div>
   );
 }
