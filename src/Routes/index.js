d1c74a391ac045abdef1d41009f8b00df17d488d
aldeez988 <aldeez911088@gmail.com>
2019-07-27 21:21:54 +0100

    fixing bugs

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -9,6 +9,7 @@ import StudentPage from "../Components/StudentPage";
 import AdminPage from "../Components/Admin/AdminPage";
 import ProgressPage from "../Components/ProgressPage";
 import PerformancePage from "../Components/Performance";
+import Mentor from "../Components/Mentors/Mentor";
 
 export default () => (
   <Switch>
@@ -17,7 +18,7 @@ export default () => (
     <Route exact path="/sudentpage" component={StudentPage} />
     <Route exact path="/stetarget" component={SetTargetPage} />
     <Route exact path="/admin" component={AdminPage} />
-    <Route exact path="/mentor" component={ProgressPage} />
+    <Route exact path="/mentor" component={Mentor} />
     <Route exact path="/performancepage" component={PerformancePage} />
 
     {/* <Route component={NotFound} /> */}
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import Mentor from "./Components/Mentors/Mentor";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<Mentor />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
