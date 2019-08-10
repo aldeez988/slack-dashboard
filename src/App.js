dfb69dd9e70a3d80398adbe93ff267b6a7d0bd1e
aldeez988 <aldeez911088@gmail.com>
2019-08-10 20:19:22 +0100

    fixing footer correct path

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./Routes";
-import Footer from "../src/Footer/index";
+import Footer from "../src/Components/Footer/index";
 import "./App.css";
 class App extends Component {
   state = { userExist: false, userData: {} };
